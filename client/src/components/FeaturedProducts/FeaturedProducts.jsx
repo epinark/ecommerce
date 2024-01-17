29fc4604f96101ef686006fc0aecf26d65ee65e4
epinark <122348695+epinark@users.noreply.github.com>
2024-01-17 13:42:32 +0100

    add card component

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+import Card from "../Card/Card";
+
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/4393685/pexels-photo-4393685.jpeg?auto=compress&cs=tinysrgb&w=600",
+      img2: "https://images.pexels.com/photos/10802672/pexels-photo-10802672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Long Sleeve Graphic T-Shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/158648/girl-coat-old-coat-brown-coat-158648.jpeg?auto=compress&cs=tinysrgb&w=600",
+      img2: "https://images.pexels.com/photos/2146344/pexels-photo-2146344.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1427984/pexels-photo-1427984.jpeg?auto=compress&cs=tinysrgb&w=600",
+      img2: "https://images.pexels.com/photos/1425248/pexels-photo-1425248.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Skirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=600",
+      img2: "https://images.pexels.com/photos/906002/pexels-photo-906002.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Hat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+          doloribus eos velit cumque expedita eius voluptatum in dolorem
+          reprehenderit unde ea, nam soluta ut quis aperiam quia natus accusamus
+          est.
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
