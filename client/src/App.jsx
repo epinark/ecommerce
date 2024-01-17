b36ec03f9df9c585e9eca8c7492c698d4352be30
epinark <122348695+epinark@users.noreply.github.com>
2024-01-17 09:49:48 +0100

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Category</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Product</div>,
+  },
+]);
+
 function App() {
-  return <>Ecommerce</>;
+  return (
+    <>
+      {" "}
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
