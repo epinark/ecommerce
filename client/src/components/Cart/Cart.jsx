9e650ba46722141a4dc4b0f2d67b18dc822be336
epinark <122348695+epinark@users.noreply.github.com>
2024-01-20 17:37:34 +0100

    update shopping cart with react-redux

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,53 +1,46 @@
 import React from "react";
 import "./Cart.scss";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem, resetCart } from "../../redux/cartReducer";
 
 const Cart = () => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1427984/pexels-photo-1427984.jpeg?auto=compress&cs=tinysrgb&w=600",
-      img2: "https://images.pexels.com/photos/1425248/pexels-photo-1425248.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Skirt",
-      desc: "Skirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=600",
-      img2: "https://images.pexels.com/photos/906002/pexels-photo-906002.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Hat",
-      desc: "Skirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
+
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
-      {data?.map((item) => (
+      {products?.map((item) => (
         <div className="item" key={item.id}>
-          <img src={item.img} alt="" />
+          <img src={import.meta.env.VITE_APP_UPLOAD_URL + item.img} alt="" />
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc.substring(0, 100)}</p>
-            <div className="price">1 x {item.price}</div>
+            {item.quantity} x ${item.price}
           </div>
           <DeleteOutlinedIcon
             className="delete"
-            // onClick={() => dispatch(removeItem(item.id))}
+            onClick={() => dispatch(removeItem(item.id))}
           />
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
-        <span>$120</span>
+        <span>${totalPrice()}</span>
       </div>
       <button>PROCEED TO CHECKOUT</button>
-      <span className="reset">Reset Cart</span>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
   );
 };
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -6,11 +6,15 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
   const id = useParams().id;
   const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
+
+  const dispatch = useDispatch();
 
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
@@ -66,18 +70,18 @@ const Product = () => {
             </div>
             <button
               className="add"
-              // onClick={() =>
-              //   dispatch(
-              //     addToCart({
-              //       id: data.id,
-              //       title: data.attributes.title,
-              //       desc: data.attributes.desc,
-              //       price: data.attributes.price,
-              //       img: data.attributes.img.data.attributes.url,
-              //       quantity,
-              //     })
-              //   )
-              // }
+              onClick={() =>
+                dispatch(
+                  addToCart({
+                    id: data.id,
+                    title: data.attributes.title,
+                    desc: data.attributes.desc,
+                    price: data.attributes.price,
+                    img: data.attributes.img.data.attributes.url,
+                    quantity,
+                  })
+                )
+              }
             >
               <AddShoppingCartIcon /> ADD TO CART
             </button>
